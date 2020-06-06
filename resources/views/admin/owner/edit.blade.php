321e51795ffbebfc10666fe4d4f82d8b4c8ff6bc
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2020-06-06 22:36:19 +0530

    updated files

--- resources/views/admin/owner/edit.blade.php
+++ resources/views/admin/owner/edit.blade.php
@@ -9,7 +9,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a></li>
-              <li class="breadcrumb-item active">Add property owner</li>
+              <li class="breadcrumb-item active">Edit property owner</li>
             </ol>
           </div>
         </div>
