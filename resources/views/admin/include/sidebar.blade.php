1bed735137cccdcfbfd90a69bf16d45b57be8741
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2020-06-23 23:42:02 +0530

    Update sidebar.blade.php

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -282,28 +282,13 @@
               </li>
             </ul>
           </li>
-          <li class="nav-item has-treeview menu-close">
-            <a href="javascript:void(0)" class="nav-link">
+            <li class="nav-item">
+            <a href="{{route('agent.index')}}" class="nav-link">
               <i class="nav-icon fa fa-at"></i>
               <p>
                 Agents
-                <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('agent.index')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Agent List</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('agent.create')}}" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Add Agent</p>
-                </a>
-              </li>
-            </ul>
           </li>
           <li class="nav-item has-treeview menu-close">
             <a href="javascript:void(0)" class="nav-link">
