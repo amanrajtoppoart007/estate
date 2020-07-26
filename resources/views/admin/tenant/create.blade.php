6b52def522333ec9291556bae27be3c6d06391fb
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2020-07-26 22:37:57 +0530

    updated files

--- resources/views/admin/tenant/create.blade.php
+++ resources/views/admin/tenant/create.blade.php
@@ -1,4 +1,21 @@
 @extends('admin.layout.app')
+@section('breadcrumb')
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h4 class="m-0 text-dark">Add New Tenant</h4>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item active">Add New Tenant</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
 @section('content')
     <div class="card">
         <div class="card-body">
--- resources/views/admin/tenant/edit.blade.php
+++ resources/views/admin/tenant/edit.blade.php
@@ -1,4 +1,21 @@
 @extends('admin.layout.app')
+@section('breadcrumb')
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h4 class="m-0 text-dark">Edit Tenant</h4>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item active">Edit Tenant</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
 @section('content')
     <div class="card">
         <div class="card-body">
